709c86b1f0f1a35059b82ffa84cb802d35a7f031
Daniel <daniel-alh@hotmail.com>
2022-08-12 20:08:37 -0500

    Replace service worker entirely to avoid errors in the building of database

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -20,25 +20,20 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['./index.html', '/'],
+  urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-                    //d     h   m     s
-      }),
-    ],
-  });
-registerRoute();
+registerRoute(({request}) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+      cacheName: 'asset-cache',
+      plugins: [
+          new CacheableResponsePlugin({
+            statuses: [0, 200],
+          }),
+      ],
+    })
+);
